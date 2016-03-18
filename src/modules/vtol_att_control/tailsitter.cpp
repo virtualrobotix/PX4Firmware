4e7bf7019faae017e5a3c6943fe0a9f3acd4cd11
Roman <bapstr@ethz.ch>
2016-03-18 07:43:27 +0100

    disable pitch control for elevons on boxwing

--- src/modules/vtol_att_control/tailsitter.cpp
+++ src/modules/vtol_att_control/tailsitter.cpp
@@ -454,8 +454,8 @@ void Tailsitter::fill_actuator_outputs()
 			// NOTE: There is no mistake in the line below, multicopter yaw axis is controlled by elevon roll actuation!
 			_actuators_out_1->control[actuator_controls_s::INDEX_ROLL] =
 				_actuators_mc_in->control[actuator_controls_s::INDEX_YAW];	//roll elevon
-			_actuators_out_1->control[actuator_controls_s::INDEX_PITCH] =
-				_actuators_mc_in->control[actuator_controls_s::INDEX_PITCH];	//pitch elevon
+			_actuators_out_1->control[actuator_controls_s::INDEX_PITCH] = 0.0f;
+				//_actuators_mc_in->control[actuator_controls_s::INDEX_PITCH];	//pitch elevon
 		}
 
 		break;
