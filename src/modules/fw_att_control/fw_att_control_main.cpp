134bf751ec547d8510c3689545bd9ab32cbcb6aa
Roman <bapstroman@gmail.com>
2017-01-26 10:37:32 +0100

    fw_att_control: introduced max rate parameters for acro mode
    Signed-off-by: Roman <bapstroman@gmail.com>

--- src/modules/fw_att_control/fw_att_control_main.cpp
+++ src/modules/fw_att_control/fw_att_control_main.cpp
@@ -1058,6 +1058,7 @@ FixedwingAttitudeControl::task_main()
 				control_input.lock_integrator = lock_integrator;
 				control_input.groundspeed = groundspeed;
 				control_input.groundspeed_scaler = groundspeed_scaler;
+				control_input.do_turn_compensation = false;
 
 				_yaw_ctrl.set_coordinated_method(_parameters.y_coordinated_method);
 
